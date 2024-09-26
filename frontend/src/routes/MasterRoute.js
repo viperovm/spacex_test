d97c24b2177c80577a8ecb54a897602354232ce3
viperovm <viperovm@gmail.com>
2024-09-26 21:07:18 +0300

    minor update

--- frontend/src/routes/MasterRoute.js
+++ frontend/src/routes/MasterRoute.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import HomePage from "../pages/home/HomePage";
+import {useDispatch, useSelector} from "react-redux";
+import {menuAction} from "../store/actions/siteActions";
 
 const MasterRoute = () => {
+
+  const dispatch = useDispatch()
+  const { menu } = useSelector(state => state.site)
+
+  useEffect(() => {
+    if(!menu.length) {
+      dispatch(menuAction())
+    }
+  }, [menu])
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path={`/`}
+        {menu.length && menu.map(r => <Route
+          path={r.url}
           element={<HomePage/>}
-        />
+        />)}
+
       </Routes>
     </BrowserRouter>
 
